0610a8fa768aee43fceb4738346385dd5f609bb8
syedhaseeb2005 <haseebsyed444@gmail.com>
2023-05-22 22:55:22 +0500

    Todo App

--- app.js
+++ app.js
@@ -33,15 +33,23 @@ const alertshow = (alerttext) => {
 // idher hum ny edit ka fucntion chalya taky hum koi bhi item asani sy add kar sakhy
 const edititem = (uid)=>{
     submitbtn.innerText = `Edit`
+    
     const mylist = Array.from(itemlist.childNodes)
+
     editedUID = uid
     modal.classList.remove("hidden")
+
+    
     overlay.classList.remove("hidden")
+    
     const filtereddata = mylist.filter((singleitem)=>singleitem.id === uid)
+    
     inputarea.value = filtereddata[0].querySelector('p').innerText
     
     modalinput.value = filtereddata[0].querySelector('p').innerText
-    submitbtn.removeEventListener('click',submission)
+    
+    // submitbtn.removeEventListener('click',submission)
+    
     submitbtn.addEventListener('click',()=> editprocess(uid))
     // crossbtn.addEventListener('click',()=>{
     //     modal.classList.toggle('hidden')
@@ -100,7 +108,6 @@ const submission = () => {
         return
     }
     const uniqueid = new Date().getTime()
-    
     const itemdate =   `<div id="${uniqueid}" class="item">
                             <p>${inputarea.value}</p>
                             <div class="btn">
@@ -108,11 +115,16 @@ const submission = () => {
                                 <button onclick="deleteitem('${uniqueid}')">Delete</button>
                             </div>    
                         </div>`
-                        clearbtn.classList.remove("hidden")
+        clearbtn.classList.remove("hidden")
+        
         item.push(itemdate)
+        
         itemlist.innerHTML = item.join("")
+        
         alertshow(`${inputarea.value} is added to your list`)
+        
         inputarea.value = ""
+        
         // localStorage.setItem('myList' , JSON.stringify(item))
 }
 // idher hym ny clear ka fuction chalya hai taky hum item ko aik saath sab ko delete kar sakhy
@@ -120,134 +132,10 @@ const clear = () => {
     item = []
     itemlist.innerHTML = ``
     alertshow(`All item are cleared`)
-
         clearbtn.classList.add('hidden')
     
-
 }
 clearbtn.addEventListener('click', clear)
 submitbtn.addEventListener('click', submission)         
 donebtn .addEventListener("click", editprocess);
 
-
-// const c = document.body.appendChild(document.createElement('canvas')).getContext('2d')
-// const { canvas } = c
-
-// const points = []
-
-// // Properties
-// const pointsCount = 100
-// const pointRadius = 1
-// const pointVelocity = 3
-// const maxDistBetweenPoint = 150
-// const maxLineWidth = 1
-// const renderPoints = true
-// const bokehBackground = true
-// const glow = true
-
-// //
-// let frame = 0
-
-// const resize = () => {
-//    if (canvas.width !== window.innerWidth || canvas.height !== window.innerHeight) {
-//       canvas.width = window.innerWidth
-//       canvas.height = window.innerHeight
-//    }
-// }
-
-// const renderLines = (point) => {
-//    for (let i = 0; i < points.length; i++) {
-//       const _p = points[i]
-//       const _d = Math.sqrt((point.x - _p.x) ** 2 + (point.y - _p.y) ** 2)
-      
-//       if (_d > maxDistBetweenPoint) continue
-      
-//       let alpha = Math.min(1, Math.max(0, 1 - (_d / maxDistBetweenPoint)))
-      
-//       c.save()
-//       c.globalAlpha = alpha
-//       c.lineWidth = maxLineWidth * alpha
-//       c.strokeStyle = `hsl(${ point.x + point.y }deg, 100%, 50%)`
-//       c.beginPath()
-//       c.moveTo(point.x, point.y)
-//       c.lineTo(_p.x, _p.y)
-//       c.stroke()
-//       c.restore()
-//    }
-// }
-
-// const loop = () => {
-//    frame++
-//    requestAnimationFrame(loop)
-   
-//    resize()
-   
-//    c.fillStyle = "#000"
-//    c.fillRect(0, 0, canvas.width, canvas.height)
-   
-   
-//    c.fillStyle = '#fff'
-//    points.forEach(point => {
-      
-//       if (renderPoints) {
-//          c.beginPath()
-//          c.arc(point.x, point.y, pointRadius, 0, Math.PI * 2)
-//          c.fill()
-//       }
-      
-//       point.x += point.velocity.x
-//       point.y += point.velocity.y
-      
-//       if (point.x < -2) {
-//          point.x = 0
-//          point.velocity.x = -point.velocity.x
-//       }
-      
-//       if (point.x > canvas.width + 2) {
-//          point.x = canvas.width
-//          point.velocity.x = -point.velocity.x
-//       }
-      
-//       if (point.y < -2) {
-//          point.y = 0
-//          point.velocity.y = -point.velocity.y
-//       }
-      
-//       if (point.y > canvas.height + 2) {
-//          point.y = canvas.height
-//          point.velocity.y = -point.velocity.y
-//       }
-      
-//       renderLines(point)
-//    })
-   
-//    c.save()
-//    c.globalCompositeOperation = "screen"
-//    c.filter = "blur(4px)"
-//    c.drawImage(canvas, 0, 0)
-//    c.restore()
-   
-//    if (bokehBackground) {
-//       c.save()
-//       c.globalCompositeOperation = "screen"
-//       c.filter = "blur(20px)"
-//       c.scale(-1, 1)
-//       c.drawImage(canvas, -canvas.width, 0)
-//       c.restore()
-//    }
-// }
-
-// resize()
-
-// for (let i = 0; i < pointsCount; i++) {
-//    points.push({
-//       x: Math.random() * canvas.width,
-//       y: Math.random() * canvas.height,
-//       velocity: {
-//          x: -pointVelocity + Math.random() * pointVelocity * 2,
-//          y: -pointVelocity + Math.random() * pointVelocity * 2
-//       }
-//    })
-// }
-
-// loop()
